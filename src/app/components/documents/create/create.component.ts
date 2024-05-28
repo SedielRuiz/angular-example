22713e57cb5b16d72f2a243a17d711ae810c6238
mariapaulav03 <14mariapaula09@gmail.com>
2024-05-28 03:53:02 -0500

    :tada:

--- src/app/components/documents/create/create.component.ts
+++ src/app/components/documents/create/create.component.ts
@@ -1,17 +1,18 @@
-import { AfterViewInit, Component, inject, NgModule } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms'
-import { Router, RouterModule } from '@angular/router'
-import { MatDialog } from '@angular/material/dialog'
-import { MatButtonModule } from '@angular/material/button'
-import { MatCardModule } from '@angular/material/card'
-import { MatInputModule } from '@angular/material/input'
-import { ErrorDialogComponent } from '../../error-dialog/error-dialog.component'
-import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatDividerModule } from '@angular/material/divider'
-import { DocumentsService } from '../../../services/documents.service'
+import { AfterViewInit, Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
+import { DocumentsService } from '../../../services/documents.service';
 import { RelativeIdService } from '../../../services/relative-id.service';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { ErrorDialogComponent } from '../../error-dialog/error-dialog.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
     selector: 'app-create',
@@ -19,37 +20,33 @@ import { MatNativeDateModule } from '@angular/material/core';
     imports: [
         MatNativeDateModule,
         MatDatepickerModule,
-        RouterModule,
         ReactiveFormsModule,
         MatCardModule,
         MatButtonModule,
-        FormsModule,
         MatFormFieldModule,
         MatInputModule,
-        MatDividerModule
+        MatDividerModule,
+        MatSlideToggleModule,
     ],
     templateUrl: './create.component.html',
-    styleUrl: './create.component.css'
+    styleUrls: ['./create.component.css']
 })
 export class CreateDocumentComponent implements AfterViewInit {
     userId!: number;
-    documentForm : FormGroup
-    documentService = inject(DocumentsService)
+    documentForm: FormGroup;
+    documentService = inject(DocumentsService);
+    file: File | null = null;
 
     constructor(
         private router: Router,
         private dialog: MatDialog,
         private idService: RelativeIdService
     ) {
-      
         this.documentForm = new FormGroup({
-            name: new FormControl(),
-            extension: new FormControl(),
-            version: new FormControl(),
-            url: new FormControl(),
-            status: new FormControl(),
-        })
+            name: new FormControl('', { nonNullable: true }),
+        });
     }
+
     async ngAfterViewInit() {
         this.idService.currentId.subscribe(id => {
             if (id !== null) {
@@ -59,22 +56,50 @@ export class CreateDocumentComponent implements AfterViewInit {
         console.log(this.userId);
     }
 
+    onFileChange(event: any) {
+        if (event.target.files.length > 0) {
+            this.file = event.target.files[0];
+        }
+    }
+
     async create() {
         try {
-            const data = {...this.documentForm.value, status: true, employeeId: this.userId}
-            const response: any = await this.documentService.create(data)
-            console.log(response)
-            this.router.navigate(['/employee/documents/consult',this.userId])
+            if (!this.file) {
+                throw new Error('No file selected');
+            }
 
-        } catch (response: any) {
-            console.log(response.error.message)
+            const extension = this.file.name.split('.').pop();
+            if (!extension) {
+                throw new Error('File does not have an extension');
+            }
+
+            const version = 1;
+            const url = `http://example.com/${this.file.name}`;
+            const data = {
+                name: this.documentForm.get('name')?.value,
+                extension,
+                version,
+                url,
+                status: true,
+                employeeId: this.userId
+            };
+
+            console.log(data);
+
+
+            const response: any = await this.documentService.create(data);
+            console.log(response);
+            this.router.navigate(['/employee/documents/consult', this.userId]);
+        } catch (error: any) {
+            console.error(error);
+            const message = error?.response?.error?.message || 'An unexpected error occurred';
+            const statusCode = error?.response?.error?.statusCode || 'Unknown';
             this.dialog.open(ErrorDialogComponent, {
                 data: {
-                    message: response.error.message,
-                    statusCode: response.error.statusCode
+                    message,
+                    statusCode
                 }
-            })
+            });
         }
-
     }
 }
--- src/app/components/documents/update/update.component.ts
+++ src/app/components/documents/update/update.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, inject } from '@angular/core'
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
 import { ActivatedRoute, Router, RouterModule } from '@angular/router'
 import { MatDialog } from '@angular/material/dialog'
 import { MatButtonModule } from '@angular/material/button'
@@ -9,83 +9,110 @@ import { ErrorDialogComponent } from '../../error-dialog/error-dialog.component'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatDividerModule } from '@angular/material/divider'
 import { DocumentsService } from '../../../services/documents.service'
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { RelativeIdService } from '../../../services/relative-id.service';
+import { MatDatepickerModule } from '@angular/material/datepicker'
+import { MatNativeDateModule } from '@angular/material/core'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { RelativeIdService } from '../../../services/relative-id.service'
 
 @Component({
-	selector: 'app-update',
-	standalone: true,
-	imports: [
+  selector: 'app-update',
+  standalone: true,
+  imports: [
     MatNativeDateModule,
     MatDatepickerModule,
-		RouterModule,
-		ReactiveFormsModule,
-		MatCardModule,
-		MatButtonModule,
-		FormsModule,
-		MatFormFieldModule,
-		MatInputModule,
-		MatDividerModule
-	],
-	templateUrl: './update.component.html',
-	styleUrl: './update.component.css'
+    RouterModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDividerModule,
+    MatSlideToggleModule,
+  ],
+  templateUrl: './update.component.html',
+  styleUrls: ['./update.component.css']
 })
 export class UpdateDocumentComponent implements AfterViewInit {
-  userId!: number;
-	id: number = 0
-	data: any
-	documentForm : FormGroup
-    documentService = inject(DocumentsService)
+  userId!: number
+  id: number = 0
+  data: any
+  documentForm: FormGroup
+  documentService = inject(DocumentsService)
+  file: File | null = null
 
-    constructor(
-        private router: Router,
-		    private route: ActivatedRoute,
-        private dialog: MatDialog,
-        private idService: RelativeIdService
-    ) {
-		this.id = this.route.snapshot.params['id']
-		this.data = this.router.getCurrentNavigation()?.extras.state
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private dialog: MatDialog,
+    private idService: RelativeIdService
+  ) {
+    this.id = this.route.snapshot.params['id']
+    this.data = this.router.getCurrentNavigation()?.extras.state
 
-      this.documentForm = new FormGroup({
-        name: new FormControl(this.data.name),
-        extension: new FormControl(this.data.extension),
-        version: new FormControl(this.data.version),
-        url: new FormControl(this.data.url),
-        status: new FormControl(this.data.status),
+    this.documentForm = new FormGroup({
+      name: new FormControl(this.data.name, { nonNullable: true }),
+      status: new FormControl(this.data.status, { nonNullable: true }),
     })
+  }
 
+  async ngAfterViewInit() {
+    this.idService.currentId.subscribe(id => {
+      if (id !== null) {
+        this.userId = id
+      }
+    })
+  }
+
+  onFileChange(event: any) {
+    if (event.target.files.length > 0) {
+      this.file = event.target.files[0]
     }
+  }
 
-    async ngAfterViewInit() {
-        this.idService.currentId.subscribe(id => {
-            if (id !== null) {
-                this.userId = id;
-            }
-        });
-    }
+  async update() {
+    try {
+      const name = this.documentForm.get('name')?.value || ''
+      const status = this.documentForm.get('status')?.value ?? true
+      let dataToUpdate: any = { name, status }
 
-    async update() {
-        try {
-            const dataUpdate = this.documentForm.value;
-
-            const response: any = await this.documentService.update(this.id, {...dataUpdate, status:true})
-            console.log(response)
-            this.router.navigate(['/employee/documents/consult', this.userId])
-
-        } catch (response: any) {
-            console.log(response.error.message)
-            this.dialog.open(ErrorDialogComponent, {
-                data: {
-                    message: response.error.message,
-                    statusCode: response.error.statusCode
-                }
-            })
+      if (this.file) {
+        const extension = this.file.name.split('.').pop()
+        if (!extension) {
+          throw new Error('File does not have an extension')
         }
 
-    }
+        const version = this.data.version + 1
+        const url = `http://example.com/${this.file.name}`
 
-    cancelUpdateDocument() {
-        this.router.navigate(['/employee/documents/consult', this.userId]);
+        dataToUpdate = {
+          ...dataToUpdate,
+          extension,
+          version,
+          url,
+          employeeId: this.userId
+        }
+      }
+
+      console.log(dataToUpdate)
+
+      const response: any = await this.documentService.update(this.id, dataToUpdate)
+      console.log(response)
+      this.router.navigate(['/employee/documents/consult', this.userId])
+    } catch (error: any) {
+      console.error(error)
+      const message = error?.message || 'An unexpected error occurred'
+      const statusCode = error?.statusCode || 'Unknown'
+      this.dialog.open(ErrorDialogComponent, {
+        data: {
+          message,
+          statusCode
+        }
+      })
     }
+  }
+
+
+  cancelUpdateDocument() {
+    this.router.navigate(['/employee/documents/consult', this.userId])
+  }
 }
